530cde17dfd6bebdb2a4b287da12e1d94a8813fb
oneturkmen <17970732+oneturkmen@users.noreply.github.com>
2019-07-20 21:50:45 -0600

    Complete basic functionality + Travis CI

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,30 @@ mod tests {
     use crate::segment_tree::Reducer;
 
     #[test]
-    fn test_handling_bad_boundaries() {
-        assert!(true);
+    fn test_update() {
+
     }
 
     #[test]
-    fn test_handling_empty_storage() {
-        assert!(true);
+    fn test_querying() {
+        // Testing querying functionality 
+
+        // Setup
+        let vec = [2.5, 3.25, 7.0, 1.75, 9.35, 0.90];
+
+        impl Reducer for f32 {
+            fn reduce(&self, other: f32) -> f32 {
+                self + other
+            }
+        }
+
+        let tree = SegmentTree::<f32>::build(&vec);
+
+        // Run
+        let result = tree.query(0, 5).unwrap(); // inclusive, indexing = 0, 1, ..., n-1
+
+        // Check
+        assert_eq!(result, 24.75);
     }
 
     #[test]
@@ -23,7 +40,7 @@ mod tests {
         let valid_storage = [22, 12, 10, 5, 7, 10, 0, 2, 3, 0, 0, 1, 9, 0, 0];
 
         impl Reducer for i32 {
-            fn reduce(&self, other: &i32) -> i32 {
+            fn reduce(&self, other: i32) -> i32 {
                 self + other
             }
         }
@@ -54,89 +71,133 @@ mod tests {
 //    of queries (e.g. 10, 1000, and 50000)
 pub mod segment_tree {
 
-    use std::error::Error;
+    use std::fmt::Debug;
 
     pub trait Reducer: Clone + Default {
-        fn reduce(&self, other: &Self) -> Self;
+        fn reduce(&self, other: Self) -> Self;
     }
 
     fn get_mid(start: usize, end: usize) -> usize {
-        return start + (end - start) / 2;
+        return start + ((end - start) / 2);
     }
 
     pub struct SegmentTree<T> {
         storage: Vec<T>,
+        num_of_elements: usize, // number of elements
         // TODO:
         // reducer: Fn(T, T) -> T (monad?)
     }
 
     impl<T> SegmentTree<T>
-        where T: Reducer
+        where T: Reducer + Debug
     {
             fn query_helper(
                 &self,
-                left: usize,
-                right: usize,
+                start: usize,
+                end: usize,
                 from: usize,
                 to: usize,
                 i: usize,
-            ) -> Result<T, &'static str> {
+            ) -> T {
 
+                println!("Index = {} & {}", start, end);
                 // Returns element if within query range
-                if from >= left && right <= to {
-                    return Ok(self.storage[i].clone());
+                if start >= from && end <= to {
+                    println!("Returning {:?}", &self.storage[i]);
+                    return self.storage[i].clone();
                 }
 
                 // Returns default value if out of query range
-                if from < left || right > to {
-                    return Ok(Default::default());
+                if end < from || start > to {
+                    println!("Returning default");
+                    return Default::default();
                 }
 
                 // Divide and conquer!
-                let mid = get_mid(left, right);
+                let mid = get_mid(start, end);
 
-                let result: T =
-                    SegmentTree::query_helper(&self, left, mid, from, to, (i << 1) + 1)
-                    .unwrap();
+                println!("Going deeper ...");
 
-                let result = result.reduce(
-                    &SegmentTree::query_helper(&self, mid + 1, right, from, to, (i << 2) + 2)
-                    .unwrap()
-                );
+                self.query_helper(start, mid, from, to, (i << 1) + 1)
+                    .reduce(self.query_helper(mid + 1, end, from, to, (i << 1) + 2))
 
-                Ok(result)
+
+                /*let left_result: T =
+                    self.query_helper(start, mid, from, to, (i << 1) + 1);
+
+
+                /*println!("hooooooooooooooooowdddyyyyyyyyyyyyyyyyyyyyyyyyy");
+                println!("result left = {:?}", &left_result);*/
+
+                let right_result = 
+                    self.query_helper(mid + 1, end, from, to, (i << 1) + 2);
+
+                println!("result right = {:?}", &right_result);*/
+
+                //left_result.reduce(&right_result)
             }
 
             pub fn query(&self, from: usize, to: usize) -> Result<T, &'static str> {
-                let tree_size = self.storage.len();
+                let num_of_elements = self.num_of_elements;
 
                 // Sanity check
-                if from >= tree_size { // usize is always unsigned (non-negative)
+                if from >= num_of_elements { // usize is always unsigned (non-negative)
                     panic!("_from_ index is out of bounds!");
                 }
-                if to >= tree_size {
+                if to >= num_of_elements {
                     panic!("_to_ index is out of bounds!");
                 }
                 if from > to {
                     panic!("_from_ index cannot be greater than _to_ index");
                 }
 
-                match self.query_helper(0, tree_size - 1, from, to, 0) {
-                    Ok(acc) => Ok(acc),
-                    Err(e) => {
-                        println!("Oops, something went wrong! {}", e);
-                        Err(e)
-                    }
-                }
+                Ok(self.query_helper(0, num_of_elements - 1, from, to, 0))
             }
 
-            fn update_helper(&mut self, new_val: T, index: usize) {
-                unimplemented!();
+            fn update_helper(
+                &mut self,
+                new_val: &T,
+                i: usize,
+                start: usize,
+                end: usize,
+                t_i: usize,
+            ) -> Result<(), &'static str> {
+                if start > end || start > i || i > end {
+                    return Ok(());
+                }
+
+                if start == end {
+                    self.storage[t_i] = new_val.clone();
+                    return Ok(());
+                } 
+
+                let mid = get_mid(start, end);
+                
+                match self.update_helper(&new_val, i, start, mid, (t_i << 1) + 1) {
+                    Ok(_) => { }, // does nothing
+                    Err(e) => return Err(e),
+                }
+
+                match self.update_helper(&new_val, i, mid + 1, end, (t_i << 1) + 2) {
+                    Ok(_) => { },
+                    Err(e) => return Err(e),
+                }
+                
+                self.storage[t_i] = self.storage[(t_i << 1) + 1].reduce(self.storage[(t_i << 1) + 2].clone());
+
+                Ok(())
             }
 
             // TODO: Updating values in vector
-            pub fn update(&mut self, new_val: T, index: usize) {
-                unimplemented!();
+            pub fn update(&mut self, new_val: &T, i: usize) -> Result<(), &'static str> {
+                if i >= self.storage.len() {
+                    panic!("Provided index is out of bounds!");
+                }
+
+                match self.update_helper(new_val, i, 0, self.storage.len() - 1, 0) {
+                    Ok(_) => Ok(()),
+                    Err(e) => Err(e),
+                }
             }
 
             fn build_helper(
@@ -164,7 +225,7 @@ pub mod segment_tree {
                 SegmentTree::build_helper(tree, source, (i << 1) + 2, mid + 1, end);
 
                 // Combine: TODO: create Reducer trait
-                tree.storage[i] = tree.storage[(i << 1) + 1].reduce(&tree.storage[(i << 1) + 2]);
+                tree.storage[i] = tree.storage[(i << 1) + 1].reduce(tree.storage[(i << 1) + 2].clone());
             }
 
             pub fn build(vec: &[T]) -> SegmentTree<T> {
@@ -175,6 +236,7 @@ pub mod segment_tree {
                     storage: vec![
                         Default::default(); tree_size
                     ],
+                    num_of_elements: vec.len(),
                 };
 
                 SegmentTree::build_helper(&mut tree, vec, 0, 0, vec.len() - 1);
